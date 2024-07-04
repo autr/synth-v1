f78bc147125f090ae98b98b09ef95b522cf569e4
Gilbert Sinnott <gilbert.sinnott@gmail.com>
2024-07-04 14:49:37 +0300

    fix gui, redo default patch

---
+++ _app/immutable/chunks/3-a67c8ca2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/aui/_type_/_layout.svelte-67e17278.js";export{t as component};
---
+++ _app/immutable/chunks/4-a9a4f620.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-cc1b131f.js";export{t as component};
